f164d72dc592c2aba965de993e469dd1b3e34aa7
Michal Kuklis <michal.kuklis@gmail.com>
2011-04-27 01:57:40 -0400

    formatting cleanup

--- public/js/app.js
+++ public/js/app.js
@@ -29,23 +29,23 @@
 
     // few helpers based on http://www.simonwhatley.co.uk/examples/twitter/prototype/
     parseURL: function(text) {
-	    return text.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, function(url) {
-		    return url.link(url);
-	    });
+      return text.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, function(url) {
+        return url.link(url);
+      });
     },
 
     parseUsername: function(text) {
-	    return text.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
-		    var username = u.replace("@","")
-		    return u.link("http://twitter.com/" + username);
-	    });
+      return text.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
+        var username = u.replace("@","")
+        return u.link("http://twitter.com/" + username);
+      });
     },
 
     parseHashtag: function(text) {
-	    return text.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
-		    var tag = t.replace("#","%23")
-		    return t.link("http://search.twitter.com/search?q=" + tag);
-	    });
+      return text.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
+        var tag = t.replace("#","%23")
+        return t.link("http://search.twitter.com/search?q=" + tag);
+      });
     },
 
     linkify: function() {
